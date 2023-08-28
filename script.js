53d450b159367a318bca577d5b7b4938d8286ad3
Chris Benner-Kenagy <chrisbennerkenagy@gmail.com>
2023-08-28 12:42:40 -0700

    going back to greeting / ditching previous loop attempts

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// script.js
+
 function dogLover() {
     return confirm("Do you love dogs? and want to meet Lacy?");
 }
@@ -23,9 +25,9 @@ const jokes =
         "Why do dogs like conjunctions? They just love buts",
         "How did the little Scottish dog react when he met the Loch Ness Monster, He was *Terrier-fied*",
         "Why was the dog stealing Shingles? He wanted to become a *woofer*",
-    
     ];
       
+
 jkButton.addEventListener("click", () =>
  {
     const randomIndex = Math.floor(Math.random() * jokes.length);
@@ -39,72 +41,27 @@ let positionX = 0;
 document.addEventListener('keydown', (event) =>
  {
     console.log("Key pressed:", event.key);
-    if (event.key === 'ArrowRight') 
-    {
+    if (event.key === 'ArrowRight') {
         positionX += 10;
-    } 
-    else if (event.key === 'ArrowLeft')
-    {
+    } else if (event.key === 'ArrowLeft') {
         positionX -= 10;
     }
     console.log("New positionX:", positionX);
     dog.style.left = `${positionX}px`;
 });
 
-
 function greeting() 
 {
-    let userName;
-    do 
+    let userName = prompt("What is your name?");
+    
+    if (userName !== null && userName !== "") 
     {
+        const greetingContainer = document.getElementById("greetingContainer");
+        greetingContainer.textContent = "Hello " + userName + ", welcome to Lacy's World!";
+    } else 
+    {
+        console.log("There was no input, try again");
         userName = prompt("What is your name?");
-    } 
-    while (userName === null || userName.trim() === "");
-    
-    const greetingContainer = document.getElementById("greetingContainer");
-    greetingContainer.textContent = "Hello " + userName + ", let me tell you about myself";
-    
-    // Ask the user the pet question
-    let petGuess;
-    do
-    {
-        petGuess = prompt("What is the best pet?");
-        petGuess = petGuess.toLowerCase(); // Convert to lowercase
-    } 
-    while (petGuess !== "dog");
-    
-    alert("Correct! Dogs are the best pets!");
-}
-
-document.addEventListener("DOMContentLoaded", function() 
-{
-    const displayImagesButton = document.getElementById("displayImagesButton");
-    displayImagesButton.addEventListener("click", displayFunnyRatingImages);
-});
-
-function displayFunnyRatingImages() 
-{
-    const repeatedImages = document.getElementById("repeatedImages");
-    repeatedImages.innerHTML = ''; 
-
-    let funnyRating;
-
-    do
-    {
-        funnyRating = parseInt(prompt("How funny was that joke, on a scale of 1-10"), 10);
-
-        if (isNaN(funnyRating) || funnyRating < 1 || funnyRating > 10) 
-        {
-            alert("Please enter a valid number between 1 and 10.");
-        }
-    } 
-    while (isNaN(funnyRating) || funnyRating < 1 || funnyRating > 10);
-
-    for (let i = 0; i < funnyRating; i++) 
-    {
-        const image = document.createElement("img");
-        image.src = "lacy_face.jpg";
-        image.classList.add("rating");
-        repeatedImages.appendChild(image);
+        greeting(); // Call the function recursively to ensure a valid name is provided
     }
 }
